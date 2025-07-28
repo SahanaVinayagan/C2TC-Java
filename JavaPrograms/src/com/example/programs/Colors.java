0fe2a2c1b38b8392f251a6b0869e03f66288152e
Sahana A <sahanavinayagan@gmail.com>
2025-07-28 19:23:58 +0530

    Initial commit

---
+++ JavaPrograms/src/com/example/programs/Colors.java
@@ -0,0 +1,13 @@
+package com.example.programs;
+
+public class Colors {
+
+	//public static void main(String[] args) {
+		public void colorNames()
+		{
+			System.out.println("Color : Yellow");
+		}
+
+}
+
+
---
+++ JavaPrograms/src/com/example/programs/Yellow.java
@@ -0,0 +1,10 @@
+package com.example.programs;
+
+public class Yellow {
+
+	public static void main(String[] args) {
+		Colors c=new Colors();
+		c.colorNames();
+	}
+
+}
