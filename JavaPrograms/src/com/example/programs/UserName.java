5078e8b53f0d2a0997dab6efac43388865be0793
Sahana A <sahanavinayagan@gmail.com>
2025-07-29 19:00:31 +0530

    Initial commit

---
+++ JavaPrograms/src/com/example/programs/UserName.java
@@ -0,0 +1,14 @@
+package com.example.programs;
+
+import java.util.Scanner;
+
+public class UserName {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter your name : ");
+		String n=input.nextLine();
+		System.out.println("Hello "+n);
+	}
+
+}
