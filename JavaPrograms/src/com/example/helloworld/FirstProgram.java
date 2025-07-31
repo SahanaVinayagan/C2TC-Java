5ebd33458cdcb7bda64bb0aa7463c83f245c7511
Sahana A <sahanavinayagan@gmail.com>
2025-07-31 21:00:09 +0530

    Initial commit

---
+++ JavaPrograms/src/com/example/helloworld/FirstProgram.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Welcome to java learning");
+		System.out.println("Welcome to java learning");
+		System.out.println("Welcome to java learning");
+
+	}
+
+}
