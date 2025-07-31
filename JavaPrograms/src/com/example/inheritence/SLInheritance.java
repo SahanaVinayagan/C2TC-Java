c86a4546201c26cb580f5ff29e92a3005021acf7
Sahana A <sahanavinayagan@gmail.com>
2025-07-31 19:28:34 +0530

    Initial commit

---
+++ JavaPrograms/src/com/example/inheritence/SLInheritance.java
@@ -0,0 +1,10 @@
+package com.example.inheritence;
+
+public class SLInheritance {
+
+    public static void main(String[] args) {
+            Student s1=new Student("Alice","Bagalore",123456789,6364520807L,123,"JSS");
+    System.out.println(s1);
+    }
+
+}
