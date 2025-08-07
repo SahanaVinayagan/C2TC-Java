bdc4e351585e5c81b4427d668b1cf235e8593f78
Sahana A <sahanavinayagan@gmail.com>
2025-08-07 19:04:29 +0530

    Initial commit

---
+++ JavaPrograms/src/com/example/typecasting/TypeCastingExample.java
@@ -0,0 +1,23 @@
+package com.example.typecasting;
+
+public class TypeCastingExample {
+
+	public static void main(String[] args) {
+		byte b=10;
+		int i=b;
+		System.out.println(i);
+		char c='a';
+		int i1=c;
+		System.out.println(i1);
+		float f=100.00f;
+		double d=f;
+		System.out.println(d);
+		double d1=10.234;
+		float f3=(float)d1;
+		System.out.println(f3);
+		float f1=10.01f;
+		int i2=(int)f1;
+		System.out.println(i2);
+	}
+
+}
---
+++ JavaPrograms/src/com/example/wrapperclass/WrapperClass.java
@@ -0,0 +1,19 @@
+package com.example.wrapperclass;
+
+public class WrapperClass {
+
+	public static void main(String[] args) {
+		Integer i=10;
+		System.out.println(i);
+		int b=i.intValue();
+		System.out.println(b);
+
+		int c=i;
+		System.out.println(c);
+		
+		int a=100;
+		Integer i1=a;
+		System.out.println(i1);
+	}
+
+}
