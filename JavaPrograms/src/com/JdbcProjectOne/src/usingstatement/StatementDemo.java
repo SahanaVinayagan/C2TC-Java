6b4de8fdd9991fcec3cac2833e29521488fcbd97
Sahana A <sahanavinayagan@gmail.com>
2025-09-21 15:30:53 +0530

    Initial commit

---
+++ JavaPrograms/src/com/JdbcProjectOne/src/usingstatement/StatementDemo.java
@@ -0,0 +1,14 @@
+
+package usingstatement;
+
+public class StatementDemo {
+
+	public static void main(String[] args) {
+		//StatementInterfaceDemo obj=new StatementInterfaceDemo();
+		int row=StatementInterfaceDemo.addStudent();
+		System.out.println(row +" is inserted");
+
+
+	}
+
+}
